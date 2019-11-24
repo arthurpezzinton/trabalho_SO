05e5de10766b7791ab1282cbab895f56f4130497
Arthur <arthurpezzinton@gmail.com>
2019-11-24 02:05:12 -0300

    correção do ponteiro

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -30,7 +30,7 @@ static void busy_wait (int64_t loops);
 static void real_time_sleep (int64_t num, int32_t denom);
 static void real_time_delay (int64_t num, int32_t denom);
 
-struct *thread_pointer = thread_current();
+static struct thread_pointer = thread_current();  
 
 /* Sets up the timer to interrupt TIMER_FREQ times per second,
    and registers the corresponding interrupt. */
